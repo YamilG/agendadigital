4e9f24957639adc5991683ab56e6138a8310ae25
Eduardo Irías <eduardo22i@me.com>
2015-01-14 01:41:21 -0600

    Demo

--- index.js
+++ index.js
@@ -3,8 +3,64 @@ var http = require('http');
 var fs = require('fs');
 var replace = require("replace");
 var Parse = require('parse').Parse;
-var express = require('express');
-var app = express();
+//var express = require('express');
+//var app = express();
+
+
+var server = http.createServer(function (req, res) {
+
+	parseado = url.parse(req.url, true)
+	dir = parseado.pathname.split('/')
+	
+	//res.write("<h1>"+dir[1]+"</h1>"); /\*/
+	console.log(dir[1] );
+	
+	var direct = parseado.pathname.replace(/\//, '');
+	console.log(direct );
+	if(direct.search(".png") > -1 ) {
+		var img = fs.readFileSync(direct);
+     	res.writeHead(200, {"Content-Type": "image/png" });
+     	//res.end(img, 'binary');
+		res.write(img);
+	 	res.end()
+   } else if(direct.search(".jpg") > -1 ) {
+		var img = fs.readFileSync(direct);
+     	res.writeHead(200, {"Content-Type": "image/jpg" });
+     	//res.end(img, 'binary');
+		res.write(img);
+	 	res.end()
+   } else  if (direct.search(".css") > -1 ) {
+	   
+	   fs.readFile(direct, 'utf8', function (err,data) {
+		  if (err) {
+			  return console.log(err);
+		  }
+		  res.writeHead(200, {"Content-Type": "text/css"})
+		  
+		  //console.log(data);
+		  res.write(data);
+		  res.end()
+	 	});
+
+   } else {
+
+	  fs.readFile(direct, 'utf8', function (err,data) {
+		  if (err) {
+			  return console.log(err);
+		  }
+		  res.writeHead(200, {"Content-Type": "text/html"})
+		  
+		  //console.log(data);
+		  res.write(data);
+		  res.end()
+	  });
+	
+   }
+
+
+})
+/*
+server.listen(process.env.PORT || 5000)
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
@@ -40,3 +96,4 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
+*/
