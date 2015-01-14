47ed48487d63c0ae5c1a5c57205d8d7d719a09df
Eduardo Irías <eduardo22i@me.com>
2015-01-14 02:37:33 -0600

    404 page

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ var Parse = require('parse').Parse;
 //var express = require('express');
 //var app = express();
 
+console.log("Hello Cruel World");
+console.log("Node app is running at localhost: 5000");
 
 var server = http.createServer(function (req, res) {
 
@@ -38,14 +40,22 @@ var server = http.createServer(function (req, res) {
 	 	});
 
    } else {
-
+       dir[1] = (dir[1] == "") ? "index" : dir[1] 
+       
+       fs.exists(dir[1] + ".html", function (exists) {
+            if (!exists) {
+                res.writeHead(404, {'Content-Type': 'text/html'});
+                fs.createReadStream('404.html').pipe(res);
+                return false;
+        }});
+           
 	  fs.readFile(dir[1] + ".html", 'utf8', function (err,data) {
+
 		  if (err) {
-              res.writeHead(404, {"Content-Type": "text/html"})
-              res.write("error");
-              res.end;
-			  return console.log(err);
-		  }
+              return console.log(err);
+		  } else {
+          
+          res.writeHead(200, {"Content-Type": "text/html"})
           
           var dataStr = data.toString();
           dataStr = dataStr.replace(/(\r\n|\n|\r)/gm,"");
@@ -66,7 +76,7 @@ var server = http.createServer(function (req, res) {
                   
                    success: function(events) {
                        
-                       res.writeHead(200, {"Content-Type": "text/html"})
+                       
 
 
                        var prot = "";
@@ -204,9 +214,11 @@ var server = http.createServer(function (req, res) {
           
           
           
-          }
-
           
+          }
+          
+              
+          }
           /*
 		  res.writeHead(200, {"Content-Type": "text/html"})
 		  res.write(data);
@@ -220,41 +232,3 @@ var server = http.createServer(function (req, res) {
 
 })
 server.listen(process.env.PORT || 5000)
-
-/*
-
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', function(request, response) {
-    
-    Parse.initialize("rtduL3AiJ6Gt4g74rzlmKm09i2OlJ23GJPkVmEiv", "3sKYmLhuq79HxCTpErTTJG7o9LzewszO63f1Azos");
-    
-    var wallpaper = Parse.Object.extend("Wallpaper");
-    var query = new Parse.Query(wallpaper);
-
-    var page = "<h1>Hello World!</h1>";
-    query.find({
-      success: function(users) {
-          //response.write("Hello World :) ?");
-          page = page + "<ul>";
-          for (var i = 0; i < users.length; ++i) {
-            page = page + "<li>"+ users[i].get('illustrator') + "</li>";
-          }
-          page = page + "</ul>";
-          
-          response.send(page);
-      },
-        error: function(error) {
-            response.write("Error: " + error.code + " " + error.message);
-        }
-    });
-    
-    
-    
-});
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
-});
-*/
