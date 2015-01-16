9a1098a77968a998db48145b35a7c9a2acc76ee7
Eduardo Irías <eduardo22i@me.com>
2015-01-16 02:46:47 -0600

    Revert 7909bba..4e961a7
    This rolls back to commit 7909bba5bf9ca615261c3e39489dbf56414965cf.

--- parsekeys.js
+++
@@ -1,2 +0,0 @@
-//Parse.initialize("fg0ZfyAMCK6qnm2URlPUL7cmhOHpX4JUxeX4ZHsq", "A4eeWOJvusBCL8xvYDZfsD2ETgIopkM5swzGluCa");
-this.appKeys = { "appid": "fg0ZfyAMCK6qnm2URlPUL7cmhOHpX4JUxeX4ZHsq" , "jsid" : "A4eeWOJvusBCL8xvYDZfsD2ETgIopkM5swzGluCa" }; 
