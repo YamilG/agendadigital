90c430b92e5ea08e4bb40806f7e2e7f5b40b96ab
Yamil Gonzales <yamilg@gmail.com>
2015-01-21 16:28:42 -0600

    feature event img changes if mobile
    if mobile, uses bannerMini
    if !mobile uses banner
    
    needs fix, right now it puts a string not the proper URL

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,26 @@
 $(function() {
     $(window).scroll(function() {
-    var $this = $(window);
+	    var $this = $(window);
 
-    if ($this.scrollTop() >= 100) {
-      $('#search-box').fadeIn();
+	    if ($this.scrollTop() >= 100) {
+	      $('#search-box').fadeIn();
+	    }
+	});
+	 
+    window.onresize = function() {
+		checkMobile();	
     }
-});
+
+	function checkMobile() {
+		if( screen.width <= 768 ) {
+			$('#featured-img').attr('src','%Featured:bannerMini:image%');
+			// alert('is mobile!');
+		} else { 
+			$('#featured-img').attr('src','%Featured:banner:image%');
+			// alert('is not mobile!');
+		}
+	};
+		
+	checkMobile();
+
 });
